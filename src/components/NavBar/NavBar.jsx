43408b68dda505c89838dd6c5c076b68b37d52e0
JaviLM22 <javier.lpz.mrtn@gmail.com>
2023-10-07 22:40:02 +0200

    jobsoffers

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import './Navbar.css';
+import './NavBar.css';
 import { useGetConnectedAddress } from '../../hooks/useGetConnectedAddress'; 
 import { getNFTs } from '../../utils/getNFTs';
 import { saveObjectAsJson } from '../../utils/getFile';
